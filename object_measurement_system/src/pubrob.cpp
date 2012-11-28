9a6a3fb853cd3ecce28f3488914d19a4197f4c67
Eronatar <alex.go@hotmail.de>
2012-11-28 16:49:56 +0100

    backup .cpp files

--- object_measurement_system/src/pubrob.cpp
+++ object_measurement_system/src/pubrob.cpp
@@ -1,20 +1,66 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include <iostream>
-using namespace std;
+#include <sstream>
+#define MM 1000				// milli metres
+
+double getNextValue();
+char *dvalToString(const double val);
+
+double getNextValue()
+{
+	double x = 20.5;
+	return x;
+}
+
+char *dvalToString(const double val)
+{
+	double dummy = val;
+	char str[7];
+	int status = 2, div = 10, strp = 0;
+
+	if(val >= 100)
+		return (char *)"-1";
+	for(;;)
+	{
+		if(status == 2)
+		{
+			str[strp] = '0' + (char)(((int)dummy) / div);
+			dummy -= (str[strp] - '0') * div;
+			div /= 10;
+			if(dummy < 1)
+				--status;
+		}
+		else if(status == 0)
+		{
+			str[strp] = '0' + ((char)(dummy * 10));
+		}
+		else
+		{
+			str[strp] = '.';
+			div = 10;
+			--status;
+		}
+		//ROS_INFO("--> %c\n", *(str++));
+		strp++;
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "pubrob");
 	ros::NodeHandle nh;
-	ros::Publisher msr_pub = n.advertise<std_msgs::String>("pubrob",1000);
+	ros::Publisher msr_pub = nh.advertise<std_msgs::String>("pubrob",1000);
 	ros::Rate ls(200);
 
+	std_msgs::String msr_msg;
+	std::stringstream convs;
 	while(ros::ok())
 	{
-		msr_pub.publish("Checking values ...");
-		ROS_INFO("Information sent.");
-		ls.sleep(200);
+		convs << dvalToString(getNextValue()) << "m";
+		msr_msg.data = convs.str();
+		msr_pub.publish(msr_msg);
+		//ROS_INFO("Information sent.");
+		ls.sleep();
 	}
 	return 0;
 }
